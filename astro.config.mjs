5573cbdb191b542eaea123b8e8f3ddafe9be3d11
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-11-15 08:12:24 -0300

    Update pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://ross1996.github.io/aprendendo/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
