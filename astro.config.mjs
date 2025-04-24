608461f55851113abfb2e6d9fa71d65403a8e8bc
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2025-04-24 07:20:39 -0300

    Updated packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ross1996.github.io/aprendendo/',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [
+      tailwindcss(), // ADD THIS PLUGIN to the array
+    ],
+  }
 });
